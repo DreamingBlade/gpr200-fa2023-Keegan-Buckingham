3bbfb4374c79d3bd299934783bab595cc8e610cd
Keegan Buckingham <keegan.buckingham@mymail.champlain.edu>
2023-10-09 07:09:27 -0400

    Finishing the transformation cube assignment.

--- assignments/assignment4_transformations/main.cpp
+++ assignments/assignment4_transformations/main.cpp
@@ -8,15 +8,20 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+#include <kmb/shader.h>
 #include <ew/shader.h>
 #include <ew/ewMath/vec3.h>
 #include <ew/procGen.h>
+#include <kmb/transformations.h>
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 
 //Square aspect ratio for now. We will account for this with projection later.
 const int SCREEN_WIDTH = 720;
 const int SCREEN_HEIGHT = 720;
+
+const int NUM_CUBES = 4;
+const float DEGREES_IN_ROTATION = 360.0f;
 
 int main() {
 	printf("Initializing...");
@@ -51,10 +56,24 @@ int main() {
 	//Depth testing - required for depth sorting!
 	glEnable(GL_DEPTH_TEST);
 
-	ew::Shader shader("assets/vertexShader.vert", "assets/fragmentShader.frag");
+	kmb::Transform transforms[NUM_CUBES];
+
+	kmb::Shader shader("assets/vertexShader.vert", "assets/fragmentShader.frag");
 	
 	//Cube mesh
 	ew::Mesh cubeMesh(ew::createCube(0.5f));
+
+	for (int i = 0; i < NUM_CUBES; i++)
+	{
+		shader.use();
+		shader.setMat4("_Model", transforms[i].getModelMatrix());
+		cubeMesh.draw();
+	}
+
+	transforms[0].position = ew::Vec3(-.5, .5, 0);
+	transforms[1].position = ew::Vec3(-.5, -.5, 0);
+	transforms[2].position = ew::Vec3(.5, .5, 0);
+	transforms[3].position = ew::Vec3(.5, -.5, 0);
 	
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -63,11 +82,12 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		//Set uniforms
-		shader.use();
-
-		//TODO: Set model matrix uniform
-
-		cubeMesh.draw();
+		for (int i = 0; i < NUM_CUBES; i++)
+		{
+			shader.use();
+			shader.setMat4("_Model", transforms[i].getModelMatrix());
+			cubeMesh.draw();
+		}
 
 		//Render UI
 		{
@@ -76,6 +96,17 @@ int main() {
 			ImGui::NewFrame();
 
 			ImGui::Begin("Transform");
+			for (size_t i = 0; i < NUM_CUBES; i++)
+			{
+				ImGui::PushID(i);
+				if (ImGui::CollapsingHeader("Transform")) {
+					ImGui::DragFloat3("Position", &transforms[i].position.x, 0.05f);
+					ImGui::DragFloat3("Rotation", &transforms[i].rotation.x, 1.0f/DEGREES_IN_ROTATION);
+					ImGui::DragFloat3("Scale", &transforms[i].scale.x, 0.05f);
+				}
+				ImGui::PopID();
+			}
+
 			ImGui::End();
 
 			ImGui::Render();
--- core/kmb/shader.h
+++ core/kmb/shader.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <sstream>
 #include <fstream>
+
+#include "../ew/ewMath/mat4.h"
+#include "../ew/ewMath/vec3.h"
 namespace kmb
 {
 	std::string loadShaderSourceFromFile(const std::string& filePath);
@@ -14,6 +17,7 @@ namespace kmb
 		void setVec2(const std::string& name, float x, float y) const;
 		void setVec3(const std::string& name, float x, float y, float z) const;
 		void setVec4(const std::string& name, float x, float y, float z, float w) const;
+		void setMat4(const std::string& name, const ew::Mat4& v) const;
 	private:
 		unsigned int m_id; //OpenGL program handle
 	};
