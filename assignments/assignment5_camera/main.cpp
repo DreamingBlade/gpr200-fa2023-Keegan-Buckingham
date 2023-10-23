51081738846f531a45fcb3a90c875bbf0d32719a
Keegan Buckingham <keegan.buckingham@mymail.champlain.edu>
2023-10-23 15:30:46 -0400

    Trying some stuff to make the camera rotation work.

--- assignments/assignment5_camera/main.cpp
+++ assignments/assignment5_camera/main.cpp
@@ -18,6 +18,7 @@
 #include <kmb/camera.h>
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+void moveCamera(GLFWwindow* window, kmb::Camera* camera, kmb::CameraControls* controls);
 
 //Projection will account for aspect ratio!
 const int SCREEN_WIDTH = 1080;
@@ -81,8 +82,12 @@ int main() {
 	camera.orthographic = false;
 	camera.aspectRatio = static_cast<float>(SCREEN_WIDTH)/static_cast<float>(SCREEN_HEIGHT);
 
-	while (!glfwWindowShouldClose(window)) {
+	kmb::CameraControls cameraControls;
+	while (!glfwWindowShouldClose(window))
+	{
 		glfwPollEvents();
+		moveCamera(window, &camera, &cameraControls);
+
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		//Clear both color buffer AND depth buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -127,5 +132,72 @@ int main() {
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+void moveCamera(GLFWwindow* window, kmb::Camera* camera, kmb::CameraControls* controls)
+{
+	//If right mouse is not held, release cursor and return early.
+	if (!glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2))
+	{
+		//Release cursor
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+		controls->firstMouse = true;
+		return;
+	}
+	//GLFW_CURSOR_DISABLED hides the cursor, but the position will still be changed as we move our mouse.
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	//Get screen mouse position this frame
+	double mouseX, mouseY;
+	glfwGetCursorPos(window, &mouseX, &mouseY);
+
+	//If we just started right clicking, set prevMouse values to current position.
+	//This prevents a bug where the camera moves as soon as we click.
+	if (controls->firstMouse)
+	{
+		controls->firstMouse = false;
+		controls->prevMouseX = mouseX;
+		controls->prevMouseY = mouseY;
+	}
+
+	//TODO: Get mouse position delta for this frame
+	double mouseDeltaX = mouseX - controls->prevMouseX;
+	double mouseDeltaY = mouseY - controls->prevMouseY;
+	//TODO: Add to yaw and pitch
+	controls->pitch = sin(mouseDeltaY)/cos(mouseDeltaY);
+	controls->yaw = sin(mouseDeltaX)/cos(mouseDeltaX);
+	//TODO: Clamp pitch between -89 and 89 degrees
+	if (controls->pitch >= 89)
+	{
+		controls->pitch = 89;
+	}
+	else if (controls->pitch <= -89)
+	{
+		controls->pitch = -89;
+	}
+
+	//Remember previous mouse position
+	controls->prevMouseX = mouseX;
+	controls->prevMouseY = mouseY;
+
+	//Construct forward vector using yaw and pitch. Don't forget to convert to radians!
+	ew::Vec3 forward = (cos(controls->yaw*180/3.14) * cos(controls->pitch * 180 / 3.14), sin(controls->pitch * 180 / 3.14), sin(controls->yaw * 180 / 3.14)*cos(controls->pitch * 180 / 3.14));
+	//By setting target to a point in front of the camera along its forward direction, our LookAt will be updated accordingly when rendering.
+	camera->target = camera->position + forward;
+
+	//moveCamera...
+//TODO: Using camera forward and world up (0,1,0), construct camera right and up vectors. Graham-schmidt process!
+	ew::Vec3 right = (0, 0, 0);
+	ew::Vec3 up = (0, 0, 0);
+	//TODO: Keyboard controls for moving along forward, back, right, left, up, and down. See Requirements for key mappings.
+	//EXAMPLE: Moving along forward axis if W is held.
+	//Note that this is framerate dependent, and will be very fast until you scale by deltaTime. See the next section.
+	if (glfwGetKey(window, GLFW_KEY_W)) {
+		camera->position += forward * controls->moveSpeed;
+	}
+
+	//Setting camera.target should be done after changing position. Otherwise, it will use camera.position from the previous frame and lag behind
+	camera->target = camera->position + forward;
+
 }
 
