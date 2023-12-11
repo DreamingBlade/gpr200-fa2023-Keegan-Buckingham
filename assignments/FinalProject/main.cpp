36a4878c8d9339cf9fe8b7b47efad7db8f834580
Isaac HesselRobinson <isaac.hesselrobinson@mymail.champlain.edu>
2023-12-10 20:39:08 -0500

    --Cat walks clockwise around the screen

--- assignments/FinalProject/main.cpp
+++ assignments/FinalProject/main.cpp
@@ -31,11 +31,13 @@ enum Direction
 
 unsigned int createVAO(Vertex* vertexData, int numVertices, unsigned short* indicesData, int numIndices);
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+ew::Vec2 move(Direction direction, ew::Vec2 position, float distance);
+Direction checkDirection(Direction direction, ew::Vec2 position);
 
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
-const float SPRITE_WIDTH = 31;
-const float SPRITE_HEIGHT = 31;
+const float SPRITE_WIDTH = 32;
+const float SPRITE_HEIGHT = 32;
 const float CHARACTER_SPRITES_X = 3;
 const float CHARACTER_SPRITES_Y = 4;
 const float NUM_CHARACTERS_ROW = 4;
@@ -43,10 +45,12 @@ const float NUM_ROWS = 2;
 //size of the overall spritesheet
 const float IMG_SPRITES_X = CHARACTER_SPRITES_X * NUM_CHARACTERS_ROW;
 const float IMG_SPRITES_Y = CHARACTER_SPRITES_Y * NUM_ROWS;
-const float IMG_WIDTH = IMG_SPRITES_X * SPRITE_WIDTH; // 381;
-const float IMG_HEIGHT = IMG_SPRITES_Y * SPRITE_HEIGHT; // 253;
+const float IMG_WIDTH = IMG_SPRITES_X * SPRITE_WIDTH; // 384;
+const float IMG_HEIGHT = IMG_SPRITES_Y * SPRITE_HEIGHT; // 256;
 //How many seconds to stay on each animation frame for
-const float FRAME_LENGTH = 0.3f;
+const float FRAME_LENGTH = 0.2f;
+const float MOVE_SPEED = 0.15f;
+
 
 Vertex vertices[4] =
 {
@@ -110,8 +114,9 @@ int main()
 	backgroundShader.setInt("_BrickTexture", 0);
 
 	float pixelX = 1.0f / IMG_WIDTH;
-	float halfPixelX = pixelX / 2.0f;
 	float pixelY = 1.0f / IMG_HEIGHT;
+	//Never used
+	float halfPixelX = pixelX / 2.0f;
 	float halfPixelY = pixelY / 2.0f;
 
 	
@@ -135,14 +140,20 @@ int main()
 	//Make sampler2D _CharacterTexture sample from unit 1
 	characterShader.setInt("_CharacterTexture", 1);
 
-	float frameTime = 0;
+
+	//Which cat we're displaying
+	ew::Vec2 catIndex = ew::Vec2(0, 1);
 
 	int direction = (int)RIGHT;
 	int frame = 0;
 
+	ew::Vec2 characterUVPos = ew::Vec2(0.5f,0.5f);
+
 	//How much time passed since beginning of game, and how much time passed last frame. Used for deltaTime
 	float timePassed = 0;
 	float lastFrameTimePassed = 0;
+
+	float frameTime = 0;
 
 	//Main Loop
 	while (!glfwWindowShouldClose(window))
@@ -156,7 +167,8 @@ int main()
 		//Checks if you've been on this frame long enough to animate.
 		timePassed = (float)glfwGetTime();
 		//Adds delta time to frame time
-		frameTime += timePassed - lastFrameTimePassed;
+		float deltaTime = timePassed - lastFrameTimePassed;
+		frameTime += deltaTime;
 		lastFrameTimePassed = timePassed;
 		//If you have been here long enough, increments frame
 		if (frameTime >= FRAME_LENGTH)
@@ -171,7 +183,7 @@ int main()
 
 		//Draw background
 		backgroundShader.use();
-		backgroundShader.setFloat("iTime", timePassed);
+		//backgroundShader.setFloat("iTime", timePassed);
 		glBindTexture(GL_TEXTURE_2D, grassTexture);
 		//setBackgroundShaderUniforms
 		//Make sampler2D _BrickTexture sample from unit 
@@ -180,14 +192,13 @@ int main()
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, NULL);
 
 
-		//Which cat we're displaying
-		ew::Vec2 catIndex = ew::Vec2(0, 1);
 		//Which individual sprite that translates to
 		ew::Vec2 spriteIndex = ew::Vec2(catIndex.x * CHARACTER_SPRITES_X + frame, catIndex.y * CHARACTER_SPRITES_Y + (int)direction);
 		//Where on the sheet that sprite is located
 		ew::Vec2 spritePos = ew::Vec2(catSprites[(int)spriteIndex.x][(int)spriteIndex.y].x, catSprites[(int)spriteIndex.x][(int)spriteIndex.y].y);
 
-
+		characterUVPos = move((Direction)direction, characterUVPos, MOVE_SPEED * deltaTime);
+		direction = checkDirection((Direction)direction, characterUVPos);
 
 		characterShader.use();
 		characterShader.setFloat("iTime", timePassed);
@@ -195,7 +206,7 @@ int main()
 		characterShader.setInt("_CharacterTexture", 1);
 		characterShader.setFloat("_Scale", 1);
 		characterShader.setFloat("_Opacity", 1);
-		characterShader.setVec2("_CharacterPosition", 0.5,0.5);
+		characterShader.setVec2("_CharacterPosition", characterUVPos.x, characterUVPos.y);
 		characterShader.setVec2("_SpriteUVCoordinates", spritePos.x, spritePos.y);
 		characterShader.setVec2("_SpriteUVSize", SPRITE_WIDTH/IMG_WIDTH/2, SPRITE_HEIGHT/IMG_HEIGHT/2);
 
@@ -257,3 +268,46 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
+//Returns a new position in the correct direction
+ew::Vec2 move(Direction direction, ew::Vec2 position, float distance)
+{
+	if (direction == UP)
+	{
+		position.y += distance;
+	}
+	if (direction == DOWN)
+	{
+		position.y -= distance;
+	}
+	if (direction == RIGHT)
+	{
+		position.x += distance;
+	}
+	if (direction == LEFT)
+	{
+		position.x -= distance;
+	}
+	return position;
+}
+
+//Returns the direction to keep moving in
+Direction checkDirection(Direction direction, ew::Vec2 position)
+{
+	if (direction == UP && position.y > 0.9)
+	{
+		direction = RIGHT;
+	}
+	else if (direction == RIGHT && position.x > 0.9)
+	{
+		direction = DOWN;
+	}
+	if (direction == DOWN && position.y < 0.1)
+	{
+		direction = LEFT;
+	}
+	else if (direction == LEFT && position.x < 0.1)
+	{
+		direction = UP;
+	}
+	return direction;
+}
